33de8d0eb3f861a79027f43159c12adc11ce0c4a
Kevin Zhou <zzl-ko@outlook.com>
2019-12-30 15:32:36 +0800

    增加对同时链接动态库和静态库的支持

--- test_make/module.c
+++ test_make/module.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <pthread.h>
+
+void *thread_handlr(void *arg)
+{
+	printf("This thread handler function\n");
+	return NULL;
+}
 
 void module()
 {
+	int ret = 0;
+    pthread_t tid;
+
+    ret = pthread_create(&tid, NULL, thread_handlr, NULL);
+    if(0 != ret)
+    {
+        printf("pthread_create error\n");
+        return;
+    }
+    printf("Thread created succ: tid[%ld],ret[%d]\n", tid, ret);
+    //pthread_detach(tid);
+    pthread_join(tid, NULL);
+
 	printf("This should be a module of kernel!\n");
 }
