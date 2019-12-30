087dcc3e2edee2c003d9e1919b5fdab1088fec82
Kevin Zhou <zzl-ko@outlook.com>
2019-12-30 15:50:59 +0800

    增加编译生成Linux内核模块

--- test_make/module.c
+++ test_make/module.c
@@ -1,3 +1,6 @@
+#include "module.h"
+
+#ifndef KERNEL_MODULE
 #include <stdio.h>
 #include <pthread.h>
 
@@ -24,3 +27,45 @@ void module()
 
 	printf("This should be a module of kernel!\n");
 }
+
+#else // KERNEL_MODULE
+
+#include <linux/module.h>
+#include <linux/init.h>
+
+#include "calc/calc.h"
+#include "utility/utility.h"
+
+/* 设备驱动模块加载函数 */
+static int __init demo_init(void)
+{
+	int  num = 89757;
+	char str[6] = "";
+
+	printk("[kernel space]: demo_init\n");
+	my_itoa(num, str);
+	printk("[kernel space]: num[%d] -> str[%s]\n", num, str);
+	printk("[kernel space]: 10+2=%d\n", add(10,2) );
+	printk("[kernel space]: 10-2=%d\n", sub(10,2) );
+	printk("[kernel space]: 10*2=%d\n", mul(10,2) );
+	printk("[kernel space]: 10/2=%d\n", div(10,2) );
+	
+	return 0;
+}
+ 
+/* 模块卸载函数 */
+static void __exit demo_exit(void)
+{
+	printk("[kernel space]: demo_exit\n");
+	printk("[kernel space]: include header's micro is %d\n", TEST_HEADER_INCLUDE);
+}
+
+module_init(demo_init);
+module_exit(demo_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Great Hero");
+MODULE_VERSION("v1.0.0");
+MODULE_DESCRIPTION("demo");
+MODULE_ALIAS("demo");
+#endif // end KERNEL_MODULE
--- test_make/module.h
+++ test_make/module.h
@@ -4,6 +4,8 @@
 // 测试“当本头文件更新时，包含本头文件的所有源文件应当重新编译”
 #define TEST_HEADER_INCLUDE 0
 
+#ifndef KERNEL_MODULE
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,5 +15,7 @@ extern void module();
 #ifdef __cplusplus
 }
 #endif
+
+#endif // end KERNEL_MODULE
 
 #endif // end MODULE_H
