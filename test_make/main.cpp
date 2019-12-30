328332f66f92ec6997039fcc9c1603afea78ed7a
Kevin Zhou <zzl-ko@outlook.com>
2019-12-30 15:28:50 +0800

    增加对编译生成动态库或静态库的支持

--- test_make/main.cpp
+++ test_make/main.cpp
@@ -43,3 +43,20 @@ int main()
 	return 0;
 }
 #endif
+
+/* 
+动态或静态库的链接使用说明：
+1. 参见Makefile, 修改COM_TYPE变量可编译生成动态库libdemo.so或静态库libdemo.a
+2. 使用如下命令链接库生成可执行程序
+g++ main.cpp -L.bin/ -ldemo -o a.out
+3. 执行 ./a.out (如果是链接的动态库，这样执行通常会报如下错误)
+“error while loading shared libraries: libdemo.so: cannot open shared object file: No such file or directory”
+有三种解决办法，如下：
+1）指定链接库路径即可: export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./bin:.
+2）保证只有静态库存在（当动态库或静态库同时存在时，使用上述方式将默认链接动态库，程序运行时才会去加载）
+3）强制链接静态库: g++ main.cpp -L.bin/ -l:libdemo.a -o b.out
+注：相比于动态链接，使用静态链接方式编译出的程序体积将更大
+参考：（同时有.so和.a)让gcc强制链接静态库
+https://blog.csdn.net/youqika/article/details/54617525
+ */
+ 
